b876620e994cae83bedf88f267a70778196815d3
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-03-07 01:25:27 +0500

    Add jwt middleware

--- src/server.js
+++ src/server.js
@@ -1,16 +1,30 @@
 const express = require('express')
 const ejs = require('ejs')
 
+const { verify } = require('./jwt/jwt.js')
+const HOME = require('./modules/home/home.js')
+
 const run = (app) => {
 
 	app.engine('html', ejs.renderFile)
 	app.set('view engine', 'html')
 	app.use(express.static('static'))
 
-	app.get('/', (req, res) => {
+	app.use(async (req, res, next) => {
 
-		res.renderFile('index.html')
+		if(req.url === '/login' || req.url === '/register') {
+			next()
+		} else {
+			try {
+				await verify(req.headers.token)
+				next()
+			} catch(e) {
+				res.status(401).send({ error: e })
+			}
+		}
 	})
+
+	app.get('/', HOME.GET)
 
 }
 
